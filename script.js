9dc2b049eb3a4aa36c9381e54f8753fe80398154
ItsJady-Taken <davidkhun2004@gmail.com>
2024-10-22 08:42:37 +0000

    Homepage

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 
   document.addEventListener("DOMContentLoaded", function() {
     var content = document.querySelector(".projects");
-
+    var footer = document.querySelector(".footer");
     window.addEventListener("scroll", function() {
     
-        var rect = content.getBoundingClientRect();
-        var windowHeight = window.innerHeight;
+      var rect = content.getBoundingClientRect();
+      var rect2 = footer.getBoundingClientRect();
+      var windowHeight = window.innerHeight;
 
-        if (windowHeight + window.scrollY >= rect.top + rect.height) {
-            content.classList.add("loaded");
-            content.style.display = "flex";
-        }
+      if (windowHeight + window.scrollY >= rect.top + rect.height) {
+          content.classList.add("loaded");
+          content.style.display = "flex";
+      } 
+      if (windowHeight + window.scrollY >= rect2.top + rect2.height) {
+        requestAnimationFrame(() => {
+          footer.classList.add("loaded");
+        }); 
+        setTimeout(() => {
+          footer.style.display = "flex";
+        }, 100); 
+          
+      }
     })
     window.onload = function() {
         var content = document.querySelector(".projects");
         content.classList.remove("loaded");
         content.style.display = "none";
+
+        var footer = document.querySelector(".footer");
+        footer.classList.remove("loaded");
+        footer.style.display = "none";
       }
-    
 
   });
